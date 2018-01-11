4e27073bf02642724cf5138092ddf1bee13cea25
Admin <soderstromshane@gmail.com>
2018-01-11 15:59:35 -0700

    difference between var and let

---
+++ variable.js
@@ -0,0 +1,11 @@
+let age = 15;
+console.log(age);
+age = 16; //Legal
+console.log(age);
+// let age = 16; This is illegal. You can't redefine a let variable
+// var age = 16; This is illegal. You can't redifine a let variable as a var
+
+var ages = 14;
+console.log(ages);
+var ages = 13; //Legal
+console.log(ages);
