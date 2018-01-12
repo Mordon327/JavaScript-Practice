d0448e9af753e6bf5d9fa27eb7b314937dd53019
Admin <soderstromshane@gmail.com>
2018-01-11 17:09:33 -0700

    created a function that interacts with HTML DOM

---
+++ functions/functions.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var x = document.getElementById("myVar");
+  x.style.color = "red";
+}
+
+function mySecondFunction() {
+  var x = document.getElementById("myVar");
+  x.style.color = "black";
+}
